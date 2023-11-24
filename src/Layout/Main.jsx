798d34f3ee49b8690408b3e45711aaac49992a92
monira7714 <monirabegum7714@gmail.com>
2023-11-24 09:52:33 +0600

    Basic route setup is done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
+const Main = () => {
+    return (
+        <div>
+            <Button>Button</Button>
+        </div>
+    );
+};
+
+export default Main;
