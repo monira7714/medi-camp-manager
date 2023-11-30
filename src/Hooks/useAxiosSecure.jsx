5ed28907c3d38e6a3460acb47c79661e5bf9b02c
monira7714 <monirabegum7714@gmail.com>
2023-11-30 23:10:36 +0600

    The server link is replaced

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://final-assignment-server-idtdwf279-moniras-projects.vercel.app'
 })
 const useAxiosSecure = () => {
     const navigate = useNavigate();
